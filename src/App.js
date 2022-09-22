149f5a0aeaabe224367bc434c25354b9f3dce5bf
Suraj Meharwade <user@email.com>
2022-09-22 12:19:15 +0530

    Create a React app and publish it to GitHub

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ShipperList from './components/ShipperList';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className='container mh-650'>
+
+        <ShipperList />
+
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
